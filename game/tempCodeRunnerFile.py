b7f1926578f32c4ca329d860a116b7a7d9389bb6
Playm4ker <121259714+Undertaker-86@users.noreply.github.com>
2023-08-21 20:44:35 -0700

    Added time test

--- game/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-te.GroupSingle()
-player.add(Player())
--- game/test_project.py
+++ game/test_project.py
@@ -1,6 +1,17 @@
 import pytest
 from project import *
 
+def test_get_current_time():
+    global start_time
+    start_time = 0
+    # Games will terminate immediately, so current_time should be zero
+    assert get_current_time() == 0
+
+def test_display_score():
+    global start_time
+    start_time = 0
+    assert display_score() == 0
+
 def test_toggle_music():
     assert toggle_music(True) == False
     assert toggle_music(False) == True
